9efb613d3473d8f1d4a88b1e632f7991de786ef7
cagustavo11 <churaalaygustavo11@gmail.com>
2021-10-17 14:06:12 -0500

    let's make it better

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
 const hbs = require("hbs");
 const path = require("path");
 // Settings
-app.set("views", __dirname + "/views");
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-hbs.registerPartials(__dirname + "/views/partials");
-
+hbs.registerPartials(path.join(__dirname, "views/partials"));
 // middlewares
-app.use(morgan("dev"));
 app.use(
   express.urlencoded({
     extended: false,
@@ -21,8 +18,9 @@ app.use(express.static(path.join(__dirname, "../public")));
 // Router
 app.use(require("./routes/index.routes"));
 
-const PORT = process.env.PORT || 3000;
+// puerto
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(port, () => {
   console.log("I'm running! 🚀");
 });
