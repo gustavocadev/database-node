dc6b99c6e3beb1af2f5f42cef45129dbd7bdaa2b
gustavocadev <churaalaygustavo11@gmail.com>
2022-01-21 10:36:39 -0500

    I changed the dir of the readFile and writeFile

--- src/jsonMethods/readJSON.js
+++ src/jsonMethods/readJSON.js
@@ -1,8 +1,10 @@
 const fs = require("fs/promises");
+const path = require("path");
 
 const readJSON = async () => {
-    const data = await fs.readFile("./src/db.json", "utf-8");
+    const data = await fs.readFile(path.join(__dirname, "../db.json"), "utf-8");
     return JSON.parse(data);
 };
 
+console.log();
 module.exports = readJSON;
--- src/jsonMethods/writeJSON.js
+++ src/jsonMethods/writeJSON.js
@@ -4,7 +4,10 @@ const writeJSON = async (newArray) => {
     const newData = {
         users: newArray,
     };
-    await fs.writeFile("./src/db.json", JSON.stringify(newData));
+    await fs.writeFile(
+        path.join(__dirname, "../db.json"),
+        JSON.stringify(newData)
+    );
 };
 
 module.exports = writeJSON;
